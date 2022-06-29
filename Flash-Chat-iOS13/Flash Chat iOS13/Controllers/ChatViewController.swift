cf67b7e9a6733054539b897eda30605abc526b3e
woosukji <woosukji@snu.ac.kr>
2022-06-29 23:59:30 +0900

    add Flash Chat project

---
+++ Flash-Chat-iOS13/Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ChatViewController.swift
+//  Flash Chat iOS13
+//
+//  Created by Angela Yu on 21/10/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+class ChatViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var messageTextfield: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "⚡️FlashChat"
+        navigationItem.hidesBackButton = true
+    }
+    
+    @IBAction func sendPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func logoutPressed(_ sender: UIBarButtonItem) {
+        
+        do {
+            try Auth.auth().signOut()
+            navigationController?.popToRootViewController(animated: true)
+            
+        } catch let signOutError as NSError {
+          print("Error signing out: %@", signOutError)
+        }
+      
+    }
+    
+}
